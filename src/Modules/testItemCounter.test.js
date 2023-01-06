e8410e06248a8544c2e40259a0a17dedc50695d8
unknown <kashif_saleem_23@hotmail.com>
2023-01-06 16:24:34 +0500

    test case added

--- src/Modules/testItemCounter.test.js
+++ src/Modules/testItemCounter.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import displayCounter from './itemsCounter.js';
 
 function sum(a, b) {
@@ -7,3 +8,55 @@ function sum(a, b) {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+=======
+// mock the movies array
+const movies = [
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+];
+
+const movies2 = [
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the dome' },
+  { name: 'under the hood' },
+];
+
+// function to be tested
+const displayCounter = (card) => card.length;
+
+// test
+describe('movies length check', () => {
+  test('counts the number of objects in the movies array', () => {
+    expect(displayCounter(movies)).toBe(15);
+  });
+  test('counts the number of objects in the movies array', () => {
+    expect(displayCounter(movies2)).toBe(16);
+  });
+});
+>>>>>>> 5e1158a (add test for items counter)
